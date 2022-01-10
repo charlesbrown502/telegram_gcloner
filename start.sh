71985afc650d95cc957a5e2d592e9d0d53068277
charlesbrown502 <83849147+charlesbrown502@users.noreply.github.com>
2022-01-10 13:48:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
+if [[ -n $CONFIG_FILE_URL ]]; then
+	wget -q $CONFIG_FILE_URL -O /usr/src/app/telegram_gcloner/config.ini
+fi
+
 npm install http-server -g
 http-server -p $PORT &
 python3 telegram_gcloner/telegram_gcloner.py
